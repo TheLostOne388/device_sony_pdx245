3c19865ff2804151744141b50075407fb541fd52
wxo <wxo@hotmail.com>
2025-03-09 15:36:26 +0700

    Daily update: 2025-03-09 15:36:26

--- fcm_level.mk
+++ fcm_level.mk
@@ -2,22 +2,22 @@
 
 # Set FCM level in the device properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.first_api_level=33 \
-    ro.board.first.api.level=33
+    ro.product.first_api_level=35 \
+    ro.board.first.api.level=35
 
 # Set the shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 33
+PRODUCT_SHIPPING_API_LEVEL := 35
 
 # Set the framework compatibility matrix version
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.product.vndk.version=33 \
+    ro.vendor.product.vndk.version=35 \
     ro.vendor.vintf.level=15
 
 # Set the FCM level for the build system
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.api_level=33 \
-    ro.vendor.shipping_api_level=33
+    ro.vendor.api_level=35 \
+    ro.vendor.shipping_api_level=35
 
 # Define the FCM level explicitly in properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.fcm.level=15
+    ro.vendor.build.fcm.level=202404
